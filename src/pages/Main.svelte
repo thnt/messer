9c2861fd75e93724d64cc1b4edd3e443267f7daa
297mhd <90105897+297mhd@users.noreply.github.com>
2021-11-29 14:26:05 +0700

    update alert

--- src/pages/Main.svelte
+++ src/pages/Main.svelte
@@ -61,7 +61,7 @@
   getMetricPage(1);
 </script>
 
-{#if (latestTs && dayjs().diff(dayjs.unix(latestTs), 'm') > 10) || deviceErrorCode > 0}
+{#if (latestTs && dayjs().diff(dayjs.unix(latestTs), 'm') > 10) || deviceErrorCode < 0}
   <div class="alert alert-danger mb-0 bg-red text-white text-center rounded-0">
     <!-- Download SVG icon from http://tabler-icons.io/i/alert-circle -->
     <svg
